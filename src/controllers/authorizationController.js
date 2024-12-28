715f2d80e5feeb541611550ffc39dfff9ef2d57c
andres-olarte396 <andres.olarte396@gmail.com>
2024-12-28 00:19:37 -0500

    :sparkles: Agregar soporte para MongoDB y mejorar la verificación de permisos en el controlador de autorización

--- src/controllers/authorizationController.js
+++ src/controllers/authorizationController.js
@@ -1,11 +1,33 @@
-exports.checkPermission = (req, res) => {
-    const { permissions } = req.user;
+const User = require('../models/User'); // Asegúrate de tener un modelo para los usuarios
+
+exports.checkPermission = async (req, res) => {
+  try {
+    // Extraer los datos necesarios del usuario autenticado y de la solicitud
+    const userId = req.user.id;
+    console.log('userId', userId);
     const { requiredPermission } = req.body;
-  
-    if (permissions && permissions.includes(requiredPermission)) {
+
+    if (!userId || !requiredPermission) {
+      return res.status(400).json({ message: 'Faltan datos necesarios para validar el permiso' });
+    }
+
+    // Buscar al usuario en la base de datos para obtener sus permisos actualizados
+    const user = await User.findById(userId).select('permissions');
+
+    if (!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    // Validar si el usuario tiene el permiso requerido
+    const hasPermission = user.permissions.includes(requiredPermission);
+
+    if (hasPermission) {
       return res.status(200).json({ message: 'Permiso concedido' });
     }
-  
+
     return res.status(403).json({ message: 'Permiso denegado' });
-  };
-  
+  } catch (error) {
+    console.error('Error al verificar permisos:', error.message);
+    return res.status(500).json({ message: 'Error interno del servidor' });
+  }
+};
--- src/server.js
+++ src/server.js
@@ -3,18 +3,22 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authorizationRoutes = require('./routes/authorizationRoutes');
 const swaggerDocs = require('./config/swagger');
+const bodyParser = require('body-parser');
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json());
+// Middleware para analizar JSON
+app.use(bodyParser.json({ limit: '10mb' })); 
+// Ajusta el límite si necesitas más
+app.use(bodyParser.urlencoded({ extended: true }));
 
 swaggerDocs(app);
 
 app.use('/api/authorization', authorizationRoutes);
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Authorization API running on port ${PORT}`);
